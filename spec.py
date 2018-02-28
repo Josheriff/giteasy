2b8bdf45af8eeb8e64cfdf52d8f373e51361e0af
Jose <jmgallego@alea-soluciones.com>
2018-02-28 09:37:10 +0100

    This time Im doing on my way, no oop at begin, lets see if is necessary

--- spec.py
+++ spec.py
@@ -1,13 +1,22 @@
 from expects import expect, equal
 
+######### "Production" code ##########################
+
+def list_formatter(text):
+    pass
+
+
+#####################################################
+
+
+CLEAN = None
+
 SPANISH_STATUS_TEXT = """
 M  easygit.py
  M spec.py
 MM spec.py
 ?? lololo.py
 """
-
-
 
 ENGLISH_STATUS_TEXT = """
 A  easygit.py
@@ -16,7 +25,11 @@ A  easygit.py
 """
 
 with describe('Given a git status --short text'):
-    with context('it contains all files commited'):
-        with it('returns an empty list'):
+    with context('it contains nothing because all is up to date'):
+        with it('list formatter returns an empty list'):
 
+            result = list_formatter(CLEAN)
 
+            expected_result = []
+
+            expect(result).to(equal(expected_result))
